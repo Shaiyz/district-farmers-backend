198c64ba13a7c3a7dfdec1f04e40af5f08a2eba4
Shaiyz Khan <shaiyzkhan@MacBook-Pro-Shaiyz.local>
2025-03-22 11:29:45 +0500

    UPDATE: remove build files

--- dist/redis.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisClient = void 0;
-const redis_1 = require("redis");
-exports.redisClient = (0, redis_1.createClient)({
-    url: process.env.REDIS_URL || 'redis://localhost:6379'
-});
-exports.redisClient.on('error', () => console.error('Redis Error'));
